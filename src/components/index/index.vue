b6479446811517dc63eea816da540afc641437c6
xinxinmeiyouxin <2413625541@qq.com>
2024-02-01 23:08:22 +0800

    feat: 测试项目

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -7,7 +7,7 @@
         <el-container>
         <el-header>Header</el-header>
         <el-container>
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="200px"> yaAside</el-aside>
             <el-main>Main</el-main>
         </el-container>
         </el-container>
