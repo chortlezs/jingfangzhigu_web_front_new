6c0b2152ea5f3abd92779a8d02820504616e72b2
chortle_yy <2913386722@qq.com>
2024-02-01 13:22:28 +0800

    提交测试2

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import '@/assets/css/common.css'
-import '@/assets/css/index.css'
-import '@/assets/font/iconfont.css'
+
 
 createApp(App).use(router).mount('#app')
