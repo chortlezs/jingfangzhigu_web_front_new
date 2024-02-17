d8e57c1a4e1b07deeeda671d634e7ae31a7c1987
xinxinmeiyouxin <2413625541@qq.com>
2024-02-17 22:59:48 +0800

    feat: 增加弹窗内容

--- src/components/settings/popup/popup.vue
+++ src/components/settings/popup/popup.vue
@@ -11,6 +11,14 @@
       <div class="popup-name">
         <span style="font-size: 20px; color: #689AFB;">{{ props.content }}</span>
       </div>
+      <div class="popup-content">
+        <span style="margin-left: 20px;">经方智谷助手将把您与助手的对话</span>
+        <span style="margin-left: 40px;">记录和您的健康数据发送到</span> 
+        <p style="margin: 5px 0px 5px 70px; color: #689AFB;">*******@***.com邮箱</p>
+        <span style="margin-left: 75px;">请您确认信息无误！</span>
+        <button style="margin:20px 42px ;">修改</button>
+        <button style="margin:20px 35px;">确认</button>
+      </div>
     </div>
   </template>
 
@@ -39,4 +47,22 @@
     left: 210px;
   }
 
+  .popup-content {
+    position: absolute;
+    width: 311px;
+    height: 123px;
+    left: 110px;
+    top: 200px;
+    font-size: 18px;
+    button{
+        width: 78px;
+        height: 40px;
+        background-color: #689AFB;
+        color: #fff;
+        border-radius: 10px;
+        border: none;
+        font-size: 20px;
+    }
+  }
+  
   </style>
