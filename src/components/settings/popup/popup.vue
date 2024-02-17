d9f9cac6179829f70db9fbb6b7f482d2a0c46757
xinxinmeiyouxin <2413625541@qq.com>
2024-02-17 23:30:14 +0800

    feat: 增加弹窗内容

--- src/components/settings/popup/popup.vue
+++ src/components/settings/popup/popup.vue
@@ -1,7 +1,6 @@
   
 <script setup>
   import { defineProps } from 'vue';
-  
   const props = defineProps(['content']);
 </script>
 
@@ -11,12 +10,28 @@
       <div class="popup-name">
         <span style="font-size: 20px; color: #689AFB;">{{ props.content }}</span>
       </div>
-      <div class="popup-content">
+      <div class="popup-content"  v-if="props.content === '数据导出'">
         <span style="margin-left: 20px;">经方智谷助手将把您与助手的对话</span>
         <span style="margin-left: 40px;">记录和您的健康数据发送到</span> 
         <p style="margin: 5px 0px 5px 70px; color: #689AFB;">*******@***.com邮箱</p>
         <span style="margin-left: 75px;">请您确认信息无误！</span>
         <button style="margin:20px 42px ;">修改</button>
+        <button style="margin:20px 35px;">确认</button>
+      </div>
+      <div class="popup-content"  v-if="props.content === '删除账户'">
+        <span style="margin: 0px 60px;; color: #689AFB;">经方智谷助手提醒您：</span>
+        <span style="margin-left: 20px;">经方智谷助手为您的健康保驾护航，</span> 
+        <span style="margin: 0px 40px"> 您的账户一经删除无法找回！</span>
+        <span style="margin-left: 75px;">请确认是否删除！</span>
+        <button style="margin:20px 42px ;">再想想</button>
+        <button style="margin:20px 35px;">确认</button>
+      </div>
+      <div class="popup-content"  v-if="props.content === '删除所有对话'">
+        <span style="margin: 0px 60px;; color: #689AFB;">经方智谷助手提醒您：</span>
+        <span style="margin-left: 20px;">经方智谷助手为您的健康保驾护航，</span> 
+        <span style="margin: 0px 40px"> 您的账户一经删除无法找回！</span>
+        <span style="margin-left: 75px;">请确认是否删除！</span>
+        <button style="margin:20px 42px ;">再想想</button>
         <button style="margin:20px 35px;">确认</button>
       </div>
     </div>
@@ -49,7 +64,7 @@
 
   .popup-content {
     position: absolute;
-    width: 311px;
+    width: 333px;
     height: 123px;
     left: 110px;
     top: 200px;
